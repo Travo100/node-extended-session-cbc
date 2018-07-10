d0771a5bbffb5f1918f003bda83834f1944fe92a
Travo100 <travo100@gmail.com>
2018-07-10 16:50:44 -0700

    Added twitter to node application

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("NODE WORKS!");
+require('dotenv').config();
+
+var keys = require("./keys.js");
+
+var Twitter = require('twitter');
+ 
+var client = new Twitter(keys.twitter);
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
