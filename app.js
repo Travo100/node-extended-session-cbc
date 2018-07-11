93ea16ea7ec1e9fd8012e59e7554467bb751030e
Travo100 <travo100@gmail.com>
2018-07-10 17:01:47 -0700

    Got tweets from hard coded nodejs user

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var client = new Twitter(keys.twitter);
 var params = {screen_name: 'nodejs'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets);
+    console.log("Tweets from: @" + tweets[0].user.screen_name);
+    for(var i = 0; i < tweets.length; i++) {
+        console.log("---------");
+        console.log("Tweet: " + tweets[i].text);
+        console.log("Date of Tweet " + tweets[i].created_at);
+    }
   }
 });
